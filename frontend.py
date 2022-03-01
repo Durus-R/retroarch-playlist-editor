6213513ecc2426c4cadb214a36be38e4adc85e51
ronnie1320 <smartpower0105@gmail.com>
2022-03-01 18:13:23 +0100

    Final Version

--- frontend.py
+++ frontend.py
@@ -14,7 +14,17 @@ if not os.path.exists(retroarch_playlist_dir):
     retroarch_playlist_dir = os.environ["home"]
 
 
-# TODO: Let the user move items - Impossible
+
+
+class Item(QtWidgets.QListWidgetItem):
+    def __init__(self, text, parent=None, type_: int = ..., path=None):
+        super().__init__(text, parent)
+        self._path_ = path
+        self.setFlags(self.flags() | QtCore.Qt.ItemIsEditable)
+
+    def fullpath(self):
+        return self._path_
+
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -32,6 +42,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.list.move(10, 20)
         self.list.currentItemChanged.connect(self.item_changed)
         self.list.itemDoubleClicked.connect(self.edit_click)
+        self.list.setDragDropMode(QtWidgets.QAbstractItemView.InternalMove)
         # self.list.addItem("Test")
         # self.list.addItem("Test2")
 
@@ -61,9 +72,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.lblPath.setFont(QtGui.QFont("Arial", 9))
         self.lblPath.setText("")
 
-        for index in range(self.list.count()):
-            item = self.list.item(index)
-            item.setFlags(item.flags() | QtCore.Qt.ItemIsEditable)
+        self.btnRemove.setEnabled(False)
+        self.btnEdit.setEnabled(False)
+        self.btnSave.setEnabled(False)
 
     def add_click(self):
         # self.data["test"] = "test2"
@@ -74,31 +85,33 @@ class MainWindow(QtWidgets.QMainWindow):
                                                                "All Files (*)", options=options)
         if type(file_names == list):
             for i in file_names:
-                self.data[backend.generate(i)] = i
-                self.list.addItem(backend.generate(i))
+                # self.data[backend.generate(i)] = i  
+                self.list.addItem(Item(backend.generate(i), path=i))
+        elif not file_names:
+            return
         else:
             assert type(file_names) == str
-            self.data[backend.generate(file_names)] = file_names
-            self.list.addItem(backend.generate(file_names))
-        for index in range(self.list.count()):
-            item = self.list.item(index)
-            item.setFlags(item.flags() | QtCore.Qt.ItemIsEditable)
+            # self.data[backend.generate(file_names)] = file_names
+            self.list.addItem(Item(backend.generate(file_names), path=file_names))
+        self.btnSave.setEnabled(True)
 
     def remove_click(self):
         if self.list.currentItem():
-            del self.data[self.list.currentItem().text()]
+            # del self.data[self.list.currentItem().text()]
             self.list.takeItem(self.list.row(self.list.currentItem()))
+            if not self.list.count():
+                self.btnSave.setEnabled(False)
 
     def edit_click(self, _=None):
         print("Edit Click")
-        self.backup_data[self.list.currentIndex()] = self.data[self.list.currentItem().text()]
+        # self.backup_data[self.list.currentIndex()] = self.data[self.list.currentItem().text()]
         index = self.list.currentIndex()
         if index.isValid():
             item = self.list.itemFromIndex(index)
             if not item.isSelected():
                 item.setSelected(True)
             self.list.edit(index)
-        del self.data[self.list.currentItem().text()]
+        # del self.data[self.list.currentItem().text()]
 
     def save_click(self):
         options = QtWidgets.QFileDialog.Options()
@@ -106,24 +119,30 @@ class MainWindow(QtWidgets.QMainWindow):
         file_name, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Save Playlist", retroarch_playlist_dir,
                                                              "All Files (*);;Playlists (*.lpl)", options=options)
         pl = backend.Playlist()
-        for i in self.data.keys():
-            pl += [i, self.data[i]]
+        for i in range(self.list.count()):
+            pl += [self.list.item(i).text(), self.list.item(i).fullpath()]
         pl.create_json(file_name)
 
     def item_changed(self, item0, item1):
         print("Changed")
         if item0:
-            try:
-                self.lblPath.setText(self.data[item0.text()])
-                self.lblPath.setToolTip(self.data[item0.text()])
-            except KeyError:
-                self.data[item0.text()] = self.backup_data[self.list.currentIndex()]
-                self.lblPath.setText(self.data[item0.text()])
-                self.lblPath.setToolTip(self.data[item0.text()])
+            # try:
+            #     self.lblPath.setText(self.data[item0.text()])
+            #     self.lblPath.setToolTip(self.data[item0.text()])
+            # except KeyError:
+            #     self.data[item0.text()] = self.backup_data[self.list.currentIndex()]
+            #     self.lblPath.setText(self.data[item0.text()])
+            #     self.lblPath.setToolTip(self.data[item0.text()])
+            self.lblPath.setToolTip(item0.fullpath())
+            self.lblPath.setText(item0.fullpath())
+            self.btnEdit.setEnabled(True)
+            self.btnRemove.setEnabled(True)
 
         else:
             self.lblPath.setText("")
             self.lblPath.setToolTip("")
+            self.btnRemove.setEnabled(False)
+            self.btnEdit.setEnabled(False)
 
 
 if __name__ == "__main__":
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+print("\nPlease run \n\npython -m pip install -r requirements.txt \n\nfor installing dependencies.")
+setup(
+    name='retroarch-playlist-editor',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='GPL',
+    author='Ronnie1320',
+    author_email='',
+    description='An simple Editor for Retroarch Playlists.'
+)
